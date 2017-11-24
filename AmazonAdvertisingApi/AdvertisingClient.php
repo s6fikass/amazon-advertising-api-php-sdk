254ed2ec5a63f2421ed09b9f022df8cf6aeb2f86
Firas <kassawat.firas@everbrent.com>
2017-11-24 17:51:36 +0100

    fixes

---
+++ AmazonAdvertisingApi/AdvertisingClient.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: firas
+ * Date: 03.11.17
+ * Time: 17:05
+ */
+
+namespace AmazonAdvertisingApi;
+
+use AmazonAdvertisingApi\Client;
+
+require_once __DIR__ . '/../AmazonAdvertisingApi/Client.php';
+
+class AdvertisingClient extends Client
+{
+
+}
